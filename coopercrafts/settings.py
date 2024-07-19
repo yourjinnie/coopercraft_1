30cea7cef289185bc45ddcb929c85d8741e7c021
yourjinnie <yourjinnie0@gmail.com>
2024-07-19 16:47:06 +0530

    For User Autthentication and Validation.....

--- coopercrafts/settings.py
+++ coopercrafts/settings.py
@@ -82,12 +82,24 @@ WSGI_APPLICATION = 'coopercrafts.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'coopcraft',
+        'HOST': 'localhost',
+        'PORT':3306,
+        'USER':'root',
+        'PASSWORD':'Ashim@1987'
     }
 }
+
 
 
 # Password validation
