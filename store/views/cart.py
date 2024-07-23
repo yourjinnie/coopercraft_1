c73ea963d4d15506ec9ff067b421d3ccfcf42142
yourjinnie <yourjinnie0@gmail.com>
2024-07-23 17:03:59 +0530

    Fulfil first Requirments...

--- store/views/cart.py
+++ store/views/cart.py
@@ -29,37 +29,5 @@ class Cart(View):
                 cart.pop(product_id, None)
 
         request.session['cart'] = cart
-        return redirect('cart')  # Assuming 'cart' is the name of your cart URL pattern
+        return redirect('cart')
 
-
-
-
-
-
-
-
-
-# from django.views import View
-# from django.shortcuts import render,redirect
-# from store.models.products import Product
-#
-#
-# class Cart(View):
-#     def get(self,request):
-#         ids=list(request.session.get('cart').keys())
-#         products=Product.get_products_by_id(ids)
-#         context={
-#             'products':products,
-#         }
-#         return render(request,'cart.html',context)
-#     def post(self, request):
-#         product_id = request.POST.get('product_id')
-#         cart = request.session.get('cart', {})
-#
-#         if product_id in cart:
-#             cart[product_id] += 1
-#         else:
-#             cart[product_id] = 1
-#
-#         request.session['cart'] = cart
-#         return redirect('cart')  # Assuming 'cart' is the name of your cart URL pattern
