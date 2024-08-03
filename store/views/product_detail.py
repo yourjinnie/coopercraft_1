1e6fadf67e98b65e8dc14afa57e16cf6d2682397
yourjinnie <yourjinnie0@gmail.com>
2024-08-03 09:46:34 +0530

    Updated fetures...

--- store/views/product_detail.py
+++ store/views/product_detail.py
@@ -1,5 +1,5 @@
 from django.views import View
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from store.models import Product
 
 class ProductDetail(View):
--- store/views/zoom.py
+++ store/views/zoom.py
@@ -1,3 +1,24 @@
+# from django.http import JsonResponse
+# from django.views import View
+# from django.shortcuts import get_object_or_404
+# from store.models import Product
+#
+#
+# class ZoomProductView(View):
+#     def get(self, request, *args, **kwargs):
+#         product_id = self.kwargs.get('product_id')
+#         product = get_object_or_404(Product, id=product_id)
+#
+#         # Assuming your Product model has a `name` field and a related `images` field
+#         product_data = {
+#             'name': product.product_title,
+#             'image': [{'url': image.url} for image in product.image.all()],
+#         }
+#
+#         return JsonResponse(product_data)
+
+
+
 from django.views import View
 from django.shortcuts import render, get_object_or_404
 from store.models import Product
@@ -8,3 +29,12 @@ class ZoomProductView(View):
         product_id = self.kwargs.get('product_id')
         product = get_object_or_404(Product, id=product_id)
         return render(request, 'zoom_product.html', {'product': product})
+
+
+
+
+
+
+
+
+
